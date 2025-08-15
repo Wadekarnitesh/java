ae896a7d257a2c1b510753892ce27592d27928ba
nitesh wadekar <wadekarnitesh42@gmail.com>
2025-08-15 12:04:37 +0530

    Add files via upload

---
+++ src/InterviewQuestions.java
@@ -0,0 +1,25 @@
+public class InterviewQuestions {
+
+public static void main(String[] args) {
+    int a[]={3,3,6,3,9};
+    int k=a.length/2;
+    // System.out.println(k+"");
+     int count=0;
+
+    for(int i=0;i<a.length;i++){
+       
+        
+        if(a[k]!=k){
+            count++;
+            a[k]=2;
+        }
+        else{
+            break;
+        }
+         SortArray.bubbleSort(a);
+
+
+    }
+    System.out.println(count);
+}    
+}
