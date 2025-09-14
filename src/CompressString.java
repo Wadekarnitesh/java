08b8ba51922fdc8c57fef12c6b03640718be7bac
nitesh wadekar <wadekarnitesh42@gmail.com>
2025-09-14 23:07:55 +0530

    14

--- src/CompressString.java
+++ src/CompressString.java
@@ -1,3 +1,28 @@
 public class CompressString {
+
+    public static void main(String[] args) {
+        String s="aaaahhhjjjjjlll";
+        System.out.println(compressString(s));
+        
+    }
+    public static String compressString(String s){
+        String ans="";
+        int ct=1;
+        for(int i=0,j=i+1;i<s.length();){
+            if(j<s.length()&& s.charAt(i)==s.charAt(j)){
+                ct++;
+                j++;
+            }else{
+                ans=ans+s.charAt(i);
+                ans=ans+ct;
+                i=j;
+                ct=1;
+                j=i+1;
+            }
+
+        }
+        return ans;
+    }
+    
     
 }
--- src/DeCompress.java
+++ src/DeCompress.java
@@ -1,3 +1,42 @@
 public class DeCompress {
-    
+
+    public static void main(String[] args) {
+        String s = "a5u9t7";
+        System.out.println(deCompressString(s));
+    }
+
+    public static String repeaString(int num, char ch) {
+        String ans = "";
+        for (int i = 0; i < num; i++) {
+            ans += ch;
+        }
+        return ans;
+    }
+
+    public static String deCompressString(String s) {
+        String ans = "";
+        char temp = '\u0000';
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                // If we had a previous char+num pending, flush it
+                if (num > 0) {
+                    ans += repeaString(num, temp);
+                    num = 0;
+                }
+                temp = ch;
+            } 
+            else if (ch >= '0' && ch <= '9') {
+                num = num * 10 + (ch - '0'); // handles multi-digit numbers
+            }
+        }
+
+        // flush last pending repeat
+        ans += repeaString(num, temp);
+
+        return ans;
+    }
 }
