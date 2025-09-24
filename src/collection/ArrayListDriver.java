fc41bc7db196f81124eedb8cbcea510d062d3e24
nitesh wadekar <wadekarnitesh42@gmail.com>
2025-09-24 22:43:29 +0530

    .

---
+++ src/collection/ArrayListDriver.java
@@ -0,0 +1,15 @@
+package collection;
+
+public class ArrayListDriver {
+    public static void main(String[] args) {
+        ArrayList<Integer> a1=new ArrayList<>();
+        ArrayList<Integer> a2=new ArrayList<>();
+        a1.add(100);
+        a1.add(3);
+        a2.addAll((ResizableArray) a1);
+        System.out.println(a2);
+        
+        
+    }
+    
+}
---
+++ src/collection/ResizableArray.java
@@ -0,0 +1,29 @@
+package collection;
+
+public  interface  ResizableArray<E> {
+    int initialCap=10;
+    double loadFactor=0.75;
+    boolean add(E o);
+    boolean add(int index,E o);
+    boolean addAll(ResizableArray<E> arr);
+    boolean addAll(int index, ResizableArray<E> arr);
+    boolean remove(E o);
+    boolean remove(int index);
+    boolean removeAll(ResizableArray<E> arr);
+    boolean retailAll(ResizableArray<E> arr);
+    void clear();
+    int size();
+    boolean contains(E o);
+    boolean containsAll(ResizableArray<E> arr);
+    boolean isEmpty();
+    Object get(int index);
+    Object[] toArray();
+    boolean equals(Object o);
+    int hashCode();
+    String toString();
+    
+
+
+
+    
+}
